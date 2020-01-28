2d2b264690c4ff2dcadfd390123fe15d898a9cdc
jbarbic-tvz <jbarbic@tvz.hr>
2020-01-28 21:56:53 +0100

    Added report implementation for OS info and update on main menu.

--- get_os.py
+++ get_os.py
@@ -4,20 +4,25 @@
 import os
 
 def nmap_scan(report, report_path, target_ip):
-    print("DO SCAN")
-    if(report == "doreport"):
-        print("Report shall be created")
-    else:
-        print("Report shall not be created")
+    print("========== Try to get OS info ================")
+
     print("Report path is: " + report_path)
     print("Target address is: " + target_ip)
 
     command_string="nmap -O "+target_ip
 
     os.system(command_string)
-    f = os.popen(command_string)
-    raw_data = f.read()
+    fun_d = os.popen(command_string)
+    raw_data = fun_d.read()
+
     print(raw_data)
+    raw_data="========== OS info =============\n"+raw_data+"================================\n"
+
+    if(report == "doreport"):
+        print("Writing report file to: " + report_path)
+        fil_d = open(report_path, "w")
+        fil_d.write(raw_data)
+        fil_d.close()
 
 if __name__ == '__main__':
     nmap_scan()
--- main.py
+++ main.py
@@ -6,7 +6,13 @@ import operator
 # tools
 import get_os
 
+def final_report():
+    print("Do final report from sub reports...")
+
 def main():
+    # default configuration
+    report_status="doreport"
+    target_address="192.168.1.1"
     # circular menu
     subs = {}
     subs["1"]="Scan ports"
@@ -14,28 +20,41 @@ def main():
     subs["3"]="do stuf 3"
     subs["4"]="Create report"
     subs["5"]="Delete reports"
+    subs["6"]="Print current configuration"
+    subs["7"]="Set report output"
+    subs["8"]="Set target IP"
     subs["9"]="Exit"
     # repeat until 0 exit is selected
     while True:
         options=subs.keys()
-        sorted(options)
+        options=sorted(options)
         for entry in options:
             print(entry, subs[entry])
 
         selection=input("Select from listed:")
-        if selection =='1':
+        if (selection == 1):
             print("Prot scan")
             os.system("./port_scan.py arg1 arg2")
-        elif selection == '2':
+        elif (selection == 2):
             print ("Geting OS version")
-            get_os.nmap_scan("doreport","report_get_os.txt","192.168.1.71")
-        elif selection == '3':
+            get_os.nmap_scan(report_status,"report_get_os.txt",target_address)
+        elif (selection == 3):
             print("do 3")
-        elif selection == '4':
+        elif (selection == 4):
             print("Creating report")
-        elif selection == '5':
+        elif (selection == 5):
             print("Deleting reports")
-        elif selection == '0':
+        elif (selection == 6):
+            print("Current configuration:")
+            print("Report output: " + report_status)
+            print("Target address: " + target_address)
+        elif (selection == 7):
+            print("Please set report output (default doreport)")
+            report_status=input("Set [doreport | noreport]: ")
+        elif (selection == 8):
+            print("Please set target IP (default 192.168.1.1)")
+            target_address=input("Set target IP: ")
+        elif (selection == 9):
             break
         else:
             print("Unrecognized option!")
